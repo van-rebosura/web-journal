859d64e3addfc3cf5d242027f800094239f15293
Van Jacob Rebosura <rebosuravan@gmail.com>
2020-02-02 00:08:49 +0800

    add menu when hamburger is clicked

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,6 +5,12 @@ const navIcon = document.querySelector('nav .nav-item .icon');
 
 navIcon.addEventListener('click', (event) => {
   console.log('clicked!');
+  const navElement = event.srcElement.parentNode.parentNode.parentNode;
+  // using children instead of childNodes its is easier cause it ignores text childNodes
+  // console.log(navElement.children[1]);
+  const nav = document.querySelector('#nav');
+  nav.classList.toggle('shown');
+  console.log(nav);
 });
 
 // console.log(navIcon);
