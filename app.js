b09e1ff25e48b774d827b38e242ada3bb6ae320d
Van Jacob Rebosura <rebosuravan@gmail.com>
2020-02-06 13:20:34 +0800

    moved some routes to index.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const cookieSession = require('cookie-session');
 
-
 console.log('dotenv testing: ' + process.env.TESTING);
 
 // initializations
@@ -16,7 +15,9 @@ const app = express();
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.set('view engine', 'ejs');
+
 app.use(cookieSession({
   name: 'session',
   secret: 'secret key'
@@ -25,17 +26,9 @@ app.use(cookieSession({
 app.use(express.static(__dirname + '/public'));
 
 // heroku dynamic port
-let port = process.env.PORT;
-
-if (!port || port === null) {
-  //localhost port
-  port = 3000;
-}
-
+let port = (process.env.PORT) ? process.env.PORT : 3000;
 
 const users = require(__dirname + '/modules/db/users.js');
-
-const isAuthenticated = require(__dirname + '/modules/security/AuthFunction').isAuthenticated;
 
 app.listen(port, (err) => {
   if (!err) {
@@ -43,129 +36,11 @@ app.listen(port, (err) => {
   }
 });
 
-// root route
-
-app.get('/', (req, res) => {
-
-  let access = false;
-
-  if (req.session.view) {
-    console.log('session found, session id: ' + req.session.view);
-
-    let view = req.session.view;
-    users.forEach((user) => {
-      if (view === user.activeSession) {
-        console.log('/ root: found session');
-
-        let ejsVariables = {
-          fullName: user.name.fname + ' ' + user.name.lname,
-          posts: user.posts
-        }
-
-        res.render('dashboard', ejsVariables);
-
-        access = true;
-      }
-    });
-  } else {
-    console.log('no session');
-
-    access = false;
-
-  }
-
-  // if no valid cookie found, redirect to login
-  if (!access) {
-
-    let ejsVariables = {
-      email: '',
-      password: '',
-      invalidCredentials: false
-    }
-
-    res.render('login', ejsVariables);
-  }
-
-});
-
-
-// navigation routes
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-app.get('/contact', (req, res) => {
-  res.render('contact');
-});
-
-app.get('/account', (req, res) => {
-  if(isAuthenticated(req)) {
-    res.render('account');
-  } else {
-    res.redirect('/');
-  }
-});
-
-
-
-
-app.post('/login', (req, res) => {
-
-  inputEmail = req.body.email;
-  inputPassword = req.body.password;
-
-  let access = false;
-
-  let userId = '';
-
-  users.forEach((user) => {
-    if (user.email === inputEmail && user.password === inputPassword) {
-      userId = user.id;
-      access = true;
-      let date = Date.now();
-      console.log('/login access granted date: ' + date);
-      req.session.view = date + userId;
-      console.log('/login session: ' + req.session.view);
-      user.activeSession = req.session.view;
-    }
-  });
-
-  if (access) {
-
-    res.redirect('/');
-
-  } else {
-
-    // redirect back to login
-
-    let ejsVariables = {
-      email: inputEmail,
-      password: inputPassword,
-      invalidCredentials: true
-    }
-    res.render('login', ejsVariables);
-  }
-});
+// root route and navigation routes
+app.use('/', require(__dirname + '/routes/index'));
 
 // compose route
-
 app.use('/compose', require(__dirname + '/routes/compose'));
 
-
 // post route
-
 app.use('/posts', require(__dirname + '/routes/post'));
-
-
-
-// logout route
-
-app.post('/logout', (req, res) => {
-  if(isAuthenticated(req)) {
-    req.session.view = '';
-    res.redirect('/');
-  } else {
-    res.redirect('/');
-  }
-});
---
+++ routes/index.js
@@ -0,0 +1,118 @@
+const express = require('express');
+const router = express.Router();
+
+const users = require(process.cwd() + '/modules/db/users.js');
+
+const isAuthenticated = require(process.cwd() + '/modules/security/AuthFunction.js').isAuthenticated;
+
+router.get('/', (req, res) => {
+  let access = false;
+
+  if (req.session.view) {
+    console.log('session found, session id: ' + req.session.view);
+
+    let view = req.session.view;
+    users.forEach((user) => {
+      if (view === user.activeSession) {
+        console.log('/ root: found session');
+
+        let ejsVariables = {
+          fullName: user.name.fname + ' ' + user.name.lname,
+          posts: user.posts
+        }
+
+        res.render('dashboard', ejsVariables);
+
+        access = true;
+      }
+    });
+  } else {
+    console.log('no session');
+
+    access = false;
+
+  }
+
+  // if no valid cookie found, redirect to login
+  if (!access) {
+
+    let ejsVariables = {
+      email: '',
+      password: '',
+      invalidCredentials: false
+    }
+
+    res.render('login', ejsVariables);
+  }
+});
+
+// navigation routes
+
+router.get('/about', (req, res) => {
+  res.render('about');
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+router.get('/account', (req, res) => {
+  if(isAuthenticated(req)) {
+    res.render('account');
+  } else {
+    res.redirect('/');
+  }
+});
+
+
+// account access
+
+// logout route
+router.post('/logout', (req, res) => {
+  if(isAuthenticated(req)) {
+    req.session.view = '';
+    res.redirect('/');
+  } else {
+    res.redirect('/');
+  }
+});
+
+router.post('/login', (req, res) => {
+
+  inputEmail = req.body.email;
+  inputPassword = req.body.password;
+
+  let access = false;
+
+  let userId = '';
+
+  users.forEach((user) => {
+    if (user.email === inputEmail && user.password === inputPassword) {
+      userId = user.id;
+      access = true;
+      let date = Date.now();
+      console.log('/login access granted date: ' + date);
+      req.session.view = date + userId;
+      console.log('/login session: ' + req.session.view);
+      user.activeSession = req.session.view;
+    }
+  });
+
+  if (access) {
+
+    res.redirect('/');
+
+  } else {
+
+    // redirect back to login
+
+    let ejsVariables = {
+      email: inputEmail,
+      password: inputPassword,
+      invalidCredentials: true
+    }
+    res.render('login', ejsVariables);
+  }
+});
+
+module.exports = router;
